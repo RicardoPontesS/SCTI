c972b4ee17b33e697aea8a915954a26c9ea7d575
GitHub Action <action@github.com>
2024-09-22 18:59:38 +0000

    Auto-format Go code

--- src/main.go
+++ src/main.go
@@ -1,42 +1,41 @@
 package main
 
 import (
-    "SCTI/database"
-    "SCTI/fileserver"
-    "SCTI/middleware"
-    "fmt"
-    "log"
-    "net/http"
+	"SCTI/database"
+	"SCTI/fileserver"
+	"SCTI/middleware"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    err = database.OpenDatabase()
-    if err != nil {
-        log.Printf("Error connecting to postgres database\n%v", err)
-    }
-    defer database.CloseDatabase()
+	err = database.OpenDatabase()
+	if err != nil {
+		log.Printf("Error connecting to postgres database\n%v", err)
+	}
+	defer database.CloseDatabase()
 
-    fileserver.RunFileServer()
-    mux := http.NewServeMux()
-    LoadRoutes(mux)
+	fileserver.RunFileServer()
+	mux := http.NewServeMux()
+	LoadRoutes(mux)
 
-    certFile := "/etc/letsencrypt/live/sctiuenf.com.br/fullchain.pem"
-    keyFile := "/etc/letsencrypt/live/sctiuenf.com.br/privkey.pem"
+	certFile := "/etc/letsencrypt/live/sctiuenf.com.br/fullchain.pem"
+	keyFile := "/etc/letsencrypt/live/sctiuenf.com.br/privkey.pem"
 
-    server := http.Server{
-        Addr:    ":443",
-        Handler: middleware.EndpointLogging(mux),
-    }
+	server := http.Server{
+		Addr:    ":443",
+		Handler: middleware.EndpointLogging(mux),
+	}
 
-    fmt.Println("Server started at: https://sctiuenf.com.br")
-    
-    log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
+	fmt.Println("Server started at: https://sctiuenf.com.br")
+
+	log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
 }
-
