b686a005f955e4e3cd5ffd154463f449353b42b7
Ricardo <ricardowillian@hotmail.com.br>
2024-09-22 18:57:54 +0000

    Aplicação da alteração de portas e chaves ssl no código main.go após a sincronização do fork

--- src/main.go
+++ src/main.go
@@ -1,39 +1,42 @@
 package main
 
 import (
-	"SCTI/database"
-	"SCTI/fileserver"
-	"SCTI/middleware"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "SCTI/database"
+    "SCTI/fileserver"
+    "SCTI/middleware"
+    "fmt"
+    "log"
+    "net/http"
 
-	"github.com/joho/godotenv"
+    "github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	err = database.OpenDatabase()
-	if err != nil {
-		log.Printf("Error connecting to postgres database\n%v", err)
-	}
-	defer database.CloseDatabase()
+    err = database.OpenDatabase()
+    if err != nil {
+        log.Printf("Error connecting to postgres database\n%v", err)
+    }
+    defer database.CloseDatabase()
 
-	fileserver.RunFileServer()
+    fileserver.RunFileServer()
+    mux := http.NewServeMux()
+    LoadRoutes(mux)
 
-	mux := http.NewServeMux()
-	LoadRoutes(mux)
+    certFile := "/etc/letsencrypt/live/sctiuenf.com.br/fullchain.pem"
+    keyFile := "/etc/letsencrypt/live/sctiuenf.com.br/privkey.pem"
 
-	server := http.Server{
-		Addr:    ":8080", // adicione :xx na URL do .env se a porta não for a padrão do protocolo
-		Handler: middleware.EndpointLogging(mux),
-	}
+    server := http.Server{
+        Addr:    ":443",
+        Handler: middleware.EndpointLogging(mux),
+    }
 
-	fmt.Printf("Server Started at: %s\n", os.Getenv("URL"))
-	log.Fatal(server.ListenAndServe())
+    fmt.Println("Server started at: https://sctiuenf.com.br")
+    
+    log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
 }
+
